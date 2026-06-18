23317e83cea33995dc628bdedc31ef459a071e86
Akash <akash.kumar@outsmarttechnology.com>
2026-06-18 15:24:57 +0530

    38

--- src/components/sections/content/header.ts
+++ src/components/sections/content/header.ts
@@ -112,8 +112,11 @@ export const HEADER_HTML = `<div class="ekit-template-content-markup ekit-templa
 									<img class="ata-site-logo-img elementor-animation-"  src="/wp-content/uploads/2024/11/logo-1.svg" alt="default-logo"/>
 				</div>
 			</div>
-							</a>
-						</div>  
+								</a>
+								<a href="https://wa.me/1234567890" class="mobile-whatsapp-float" target="_blank" aria-label="WhatsApp Us">
+									<svg viewBox="0 0 24 24"><path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.888-.788-1.487-1.761-1.66-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z"/></svg>
+								</a>
+							</div>  
 							</div>
 				</div>
 				</div>
@@ -123,7 +126,7 @@ export const HEADER_HTML = `<div class="ekit-template-content-markup ekit-templa
 							<nav class="ekit-wid-con ekit_menu_responsive_tablet"
 			data-hamburger-icon=""
 			data-hamburger-icon-type="icon"
-			data-responsive-breakpoint="1024">
+			data-responsive-breakpoint="767">
 			            <button class="elementskit-menu-hamburger elementskit-menu-toggler"  type="button" aria-label="hamburger-icon">
                                     <span class="elementskit-menu-hamburger-icon"></span><span class="elementskit-menu-hamburger-icon"></span><span class="elementskit-menu-hamburger-icon"></span>
                             </button>
@@ -145,12 +148,16 @@ export const HEADER_HTML = `<div class="ekit-template-content-markup ekit-templa
 </li>
 <li id="menu-item-4274" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4274 nav-item elementskit-mobile-builder-content" data-vertical-menu=750px><a href="/contact-us/" class="ekit-menu-nav-link">Contact Us</a></li>
 <li id="menu-item-5317" class="mobile-menu menu-item menu-item-type-post_type menu-item-object-page menu-item-5317 nav-item elementskit-mobile-builder-content" data-vertical-menu=750px><a href="/contact-us/" class="ekit-menu-nav-link">Contact Us</a></li>
-</ul><div class="elementskit-nav-identity-panel"><button class="elementskit-menu-close elementskit-menu-toggler" type="button">X</button></div></div>
+</ul>
+										<div class="mobile-menu-quote-btn-wrapper">
+											<a href="/contact-us/" class="mobile-menu-quote-btn">Contact Us</a>
+										</div>
+										<div class="elementskit-nav-identity-panel"><button class="elementskit-menu-close elementskit-menu-toggler" type="button">X</button></div></div>
 			<div class="elementskit-menu-overlay elementskit-menu-offcanvas-elements elementskit-menu-toggler ekit-nav-menu--overlay"></div>        </nav>
 						</div>
 				</div>
 				</div>
-		<div class="elementor-element elementor-element-071fc9d e-con-full elementor-hidden-tablet elementor-hidden-mobile e-flex e-con e-child" data-id="071fc9d" data-element_type="container" data-e-type="container">
+		<div class="elementor-element elementor-element-071fc9d e-con-full elementor-hidden-mobile e-flex e-con e-child" data-id="071fc9d" data-element_type="container" data-e-type="container">
 				<div class="elementor-element elementor-element-4bcb627 elementor-align-right elementor-widget elementor-widget-button" data-id="4bcb627" data-element_type="widget" data-e-type="widget" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="button.default">
 				<div class="elementor-widget-container">
 									<div class="elementor-button-wrapper">
